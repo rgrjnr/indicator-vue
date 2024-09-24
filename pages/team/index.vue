ac5bb59e4936c0895f78fbba788830ca6b346f9d
Roger <roger@rogerjunior.com>
2024-09-24 11:55:16 +0100

    Fix Video

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -2,7 +2,7 @@
     <div class="noise">
         <span>no signal</span>
 
-        <video muted loop autoplay ref="video">
+        <video muted loop autoplay ref="video" v-if="showVideo">
             <source src="/video/noise.mp4" type="video/mp4" />
         </video>
     </div>
@@ -10,9 +10,11 @@
 
 <script setup>
 const video = ref();
+const showVideo = ref(false);
 
 onMounted(() => {
     if (process.client && window.innerWidth > 80 * 16) {
+        showVideo.value = true;
         video.value.play();
     }
 });
