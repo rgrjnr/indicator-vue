7a501e2cab571fff60d6f4da98e13468c034dcbb
Jacobo De Cal <jacobodecal@Jacobos-MacBook-Air-2.local>
2025-01-29 14:17:39 -0300

    adding google analytics

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { onMounted } from 'vue';
+onMounted(() => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-1Z13MSKSSL'; // Replace with your GA4 ID
+  document.head.appendChild(script);
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-1Z13MSKSSL', { page_path: window.location.pathname });
+  `;
+  document.head.appendChild(script2);
+});
+</script>
 <template>
 	<NuxtLayout>
 		<NuxtPage />
