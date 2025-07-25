e8915e0a1a3b99a4b22b117b003b710483c8a013
Roger <roger@rogerjunior.com>
2025-07-25 10:21:56 +0100

    Refactor Google Analytics script integration in app.vue by creating a reusable addScript function for better code organization and readability.

--- app.vue
+++ app.vue
@@ -1,22 +1,34 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted } from "vue";
+
+const addScript = ({ src, id, innerHTML }) => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = src;
+    script.id = id;
+    script.innerHTML = innerHTML;
+    document.head.appendChild(script);
+};
+
 onMounted(() => {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-1Z13MSKSSL'; // Replace with your GA4 ID
-  document.head.appendChild(script);
-  const script2 = document.createElement('script');
-  script2.innerHTML = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){ dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-1Z13MSKSSL', { page_path: window.location.pathname });
-  `;
-  document.head.appendChild(script2);
+    addScript({
+        src: "https://www.googletagmanager.com/gtag/js?id=G-1Z13MSKSSL",
+        id: "ga",
+    });
+
+    addScript({
+        id: "google-config",
+        innerHTML: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){ dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-1Z13MSKSSL', { page_path: window.location.pathname });
+    `,
+    });
 });
 </script>
 <template>
-	<NuxtLayout>
-		<NuxtPage />
-	</NuxtLayout>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
